bfbb7079c885a0fdb27e8f545aa1640e606fafd6
Omid Ashtari <omid@Omids-Mac-mini-2.local>
2019-10-08 17:28:12 -0700

    finished main protopye assignment, corrected typos in this assignment

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,6 +16,18 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(attrs) {
+  this.createdAt = attrs.createdAt;
+  this.name = attrs.name;
+  this.dimensions = attrs.dimensions;
+  this.destroy = function(){
+    return `${this.name} was removed from the game`;
+};
+}
+
+// GameObject.prototype.destroy = function(){
+//   return `${this.name} was removed from the game`;
+// };
 /*
   === CharacterStats ===
   * healthPoints
@@ -23,6 +35,20 @@
   * should inherit destroy() from GameObject's prototype
 */
 
+function CharacterStats(stats){
+  this.healthPoints = stats.healthPoints;
+  GameObject.call(this,stats);
+  this.areStats = stats.areStats;
+  this.takeDamage = function(){
+    return `${this.name} took damage`;
+  };
+};
+
+// CharacterStats.prototype.takeDamage = function(){
+//   return `${this.name} took damage`;
+// };
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
@@ -32,7 +58,21 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+function Humanoid(human){
+  this.team = human.team;
+  this.weapons = human.weapons;
+  this.language = human.language;
+  GameObject.call(this,human);
+  CharacterStats.call(this,human);
+  this.greet = function(){
+    return `${this.name} offers a greeting in ${this.language}`;
+}}
+// Humanoid.prototype.greet = function(){
+//   return `${this.name} offers a greeting in ${this.language}`;
+// }
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +81,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +142,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. In the Window/Global Objecting binding, .this refers to the object of the browser window you're using, so it's the whole of the browser window in which you're working.
-* 2. For Implicit Binding, when a function is called/invoked, the object preceding the .(dot) is what .this is referring to.
-* 3. For New Binding, in the use of a constructor function, the .this refers to the specific instance of the newly created object by the constructor.
-* 4. And for Explicit Binding, the use of all or apply on an object, the .this is explicitly defined when either method is called.
-*
-* write out a code example of each explanation above
 */
+
+
+// Window-Global Object Binding: this refers to the object of the browser window you are using, so it is the whole of the browser window in which you are working.
+// Implicit Binding: when a function is called/invoked, the object preceding the .(dot) is what .this is referring to.
+// New Binding: in the use of a constructor function, the .this refers to the specific instance of the newly created object by the constructor.
+// Explicit Binding: the use of all or apply on an object, the .this is explicitly defined when either method is called.
+
 
 // Principle 1
 
